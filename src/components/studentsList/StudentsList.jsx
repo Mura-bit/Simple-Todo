501aa90f7751e94bbce09b7f7b1132c2a238baf7
Murat.bit <mura.usa11@gmail.com>
2023-05-23 19:02:30 -0700

    cards

--- src/components/studentsList/StudentsList.jsx
+++ src/components/studentsList/StudentsList.jsx
@@ -3,15 +3,19 @@ import "./studentsList.style.css";
 
 const StudentsList = () => {
     const showStudents = students.map((el, index) => {
-        return <li key={index}>
-            {el.name}: Hobbies : {el.hobby} <img src={el.picture} />
-        </li>
+        return (
+          <div className="students-list" key={index}>
+            <img src={el.picture} />
+            <h2>{el.name}</h2>
+            <p>{el.hobby}</p>
+          </div>
+        );
     })
 
     return (
         <div className="container-app">
             <div className="students-box">
-                <ol>{showStudents}</ol>
+                {showStudents}
             </div>
         </div>
     )
