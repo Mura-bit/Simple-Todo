a52ce323c5e8d60a005c8ed4c1f840e3ed44f392
Murat.bit <mura.usa11@gmail.com>
2023-05-26 23:53:35 -0700

    jsConvert

--- src/components/carsList/carsData.js
+++ src/components/carsList/carsData.js
@@ -2,56 +2,56 @@ export const cars = [
   {
     name: "Toyota",
     description:
-      "Unleash your inner adventurer with our sleek and powerful compact car, designed for seamless city navigation and thrilling journeys.",
+      "Unleash your inner adventurer with our sleek and powerful compact car, designed for seamless city navigation.",
     picture: "https://source.unsplash.com/random/?cars&1",
     look: "Look",
   },
   {
     name: "Audi",
     description:
-      "Unleash your inner adventurer with our sleek and powerful compact car, designed for seamless city navigation and thrilling journeys.",
+      "Unleash your inner adventurer with our sleek and powerful compact car, designed for seamless city navigation.",
     picture: "https://source.unsplash.com/random/?cars&2",
     look: "Look",
   },
   {
     name: "Mercedes-Benz",
     description:
-      "Unleash your inner adventurer with our sleek and powerful compact car, designed for seamless city navigation and thrilling journeys.",
+      "Unleash your inner adventurer with our sleek and powerful compact car, designed for seamless city navigation.",
     picture: "https://source.unsplash.com/random?cars&3",
     look: "Look",
   },
   {
     name: "Lamborgini",
     description:
-      "Unleash your inner adventurer with our sleek and powerful compact car, designed for seamless city navigation and thrilling journeys.",
+      "Unleash your inner adventurer with our sleek and powerful compact car, designed for seamless city navigation.",
     picture: "https://source.unsplash.com/random?cars&4",
     look: "Look",
   },
   {
     name: "Kia",
     description:
-      "Unleash your inner adventurer with our sleek and powerful compact car, designed for seamless city navigation and thrilling journeys.",
+      "Unleash your inner adventurer with our sleek and powerful compact car, designed for seamless city navigation.",
     picture: "https://source.unsplash.com/random/?cars&5",
     look: "Look",
   },
   {
     name: "Ferrari",
     description:
-      "Unleash your inner adventurer with our sleek and powerful compact car, designed for seamless city navigation and thrilling journeys.",
+      "Unleash your inner adventurer with our sleek and powerful compact car, designed for seamless city navigation.",
     picture: "https://source.unsplash.com/random?cars&6",
     look: "Look",
   },
   {
     name: "Ford",
     description:
-      "Unleash your inner adventurer with our sleek and powerful compact car, designed for seamless city navigation and thrilling journeys.",
+      "Unleash your inner adventurer with our sleek and powerful compact car, designed for seamless city navigation.",
     picture: "https://source.unsplash.com/random?cars&7",
     look: "Look",
   },
   {
     name: "Honda",
     description:
-      "Unleash your inner adventurer with our sleek and powerful compact car, designed for seamless city navigation and thrilling journeys.",
+      "Unleash your inner adventurer with our sleek and powerful compact car, designed for seamless city navigation.",
     picture: "https://source.unsplash.com/random/?cars&8",
     look: "Look",
   },
--- src/components/studentsList/studentsData.js
+++ src/components/studentsList/studentsData.js
@@ -2,56 +2,56 @@ export const students = [
   {
     name: "Tom",
     description:
-      "Unleash your inner adventurer with our sleek and powerful compact car, designed for seamless city navigation and thrilling journeys.",
+      "Unleash your inner adventurer with our sleek and powerful compact car, designed for seamless city navigation.",
     picture: "https://source.unsplash.com/random/?portraits&1",
     look: "Look",
   },
   {
     name: "Elnura",
     description:
-      "Unleash your inner adventurer with our sleek and powerful compact car, designed for seamless city navigation and thrilling journeys.",
+      "Unleash your inner adventurer with our sleek and powerful compact car, designed for seamless city navigation.",
     picture: "https://source.unsplash.com/random/?portraits&2",
     look: "Look",
   },
   {
     name: "Aidina",
     description:
-      "bootUnleash your inner adventurer with our sleek and powerful compact car, designed for seamless city navigation and thrilling journeys.camps",
+      "bootUnleash your inner adventurer with our sleek and powerful compact car, designed for seamless city navigation.camps",
     picture: "https://source.unsplash.com/random?portraits&3",
     look: "Look",
   },
   {
     name: "Shahzod",
     description:
-      "Unleash your inner adventurer with our sleek and powerful compact car, designed for seamless city navigation and thrilling journeys.",
+      "Unleash your inner adventurer with our sleek and powerful compact car, designed for seamless city navigation.",
     picture: "https://source.unsplash.com/random?portraits&4",
     look: "Look",
   },
   {
     name: "Murat",
     description:
-      "Unleash your inner adventurer with our sleek and powerful compact car, designed for seamless city navigation and thrilling journeys.",
+      "Unleash your inner adventurer with our sleek and powerful compact car, designed for seamless city navigation.",
     picture: "https://source.unsplash.com/random/?portraits&5",
     look: "Look",
   },
   {
     name: "Esma",
     description:
-      "Unleash your inner adventurer with our sleek and powerful compact car, designed for seamless city navigation and thrilling journeys.",
+      "Unleash your inner adventurer with our sleek and powerful compact car, designed for seamless city navigation.",
     picture: "https://source.unsplash.com/random?portraits&6",
     look: "Look",
   },
   {
     name: "Alina",
     description:
-      "Unleash your inner adventurer with our sleek and powerful compact car, designed for seamless city navigation and thrilling journeys.",
+      "Unleash your inner adventurer with our sleek and powerful compact car, designed for seamless city navigation.",
     picture: "https://source.unsplash.com/random?portraits&7",
     look: "Look",
   },
   {
     name: "Tink",
     description:
-      "Unleash your inner adventurer with our sleek and powerful compact car, designed for seamless city navigation and thrilling journeys.",
+      "Unleash your inner adventurer with our sleek and powerful compact car, designed for seamless city navigation.",
     picture: "https://source.unsplash.com/random/?portraits&8",
     look: "Look",
   },
